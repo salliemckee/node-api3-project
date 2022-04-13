e85f43340b1e796aba9edf3f45b1fd0420a1e2e4
Sallie McKee <salliemckee@Morrigan.local>
2022-04-13 16:27:46 -0500

    finished middleware set up

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -36,7 +36,13 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  next();
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
